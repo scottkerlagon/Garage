a289b2de0d2a608dd5c15f5960aadc9bc66a610e
Scott Kerlagon <scud55@hotmail.com>
2019-01-22 06:50:35 -0800

    Fixed passcode and camera

--- webserverRelay.js
+++ webserverRelay.js
@@ -2,7 +2,7 @@ var NodeWebcam = require( "node-webcam" );
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
-const piGpio = require('pigpio').Gpio;
+//const piGpio = require('pigpio').Gpio;
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var relay1 = new Gpio(20, 'out'); //use GPIO pin 20 as output
 var relay2 = new Gpio(21, 'out'); //use GPIO pin 21 as output
@@ -10,30 +10,30 @@ var relay2 = new Gpio(21, 'out'); //use GPIO pin 21 as output
 //Ultra Sonic Code
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECDONDS_PER_CM = 1e6/34321;
-const trigger = new piGpio(23, {mode: piGpio.OUTPUT});
-const echo = new piGpio(24, {mode: piGpio.INPUT, alert: true});
-trigger.digitalWrite(0); // Make sure trigger is low
+//const trigger = new piGpio(7, {mode: piGpio.OUTPUT});
+//const echo = new piGpio(11, {mode: piGpio.INPUT, alert: true});
+//trigger.digitalWrite(0); // Make sure trigger is low
 
-const watchHCSR04 = () => {
-  let startTick;
+//~ const watchHCSR04 = () => {
+  //~ let startTick;
 
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-};
+  //~ echo.on('alert', (level, tick) => {
+    //~ if (level == 1) {
+      //~ startTick = tick;
+    //~ } else {
+      //~ const endTick = tick;
+      //~ const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+      //~ console.log(diff / 2 / MICROSECDONDS_PER_CM);
+    //~ }
+  //~ });
+//~ };
 
-watchHCSR04();
+//watchHCSR04();
 
 // Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+//~ setInterval(() => {
+  //~ trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+//~ }, 1000);
 
 //End Ultra Sonic Code
 
@@ -52,18 +52,21 @@ function handler (req, res) { //create server
   });
 }
 
-function getServerCode(){ 
+function getServerCode(){
 	var contents = fs.readFileSync('serverCode.txt', 'utf8');
-	serverCode = contents;
+  return contents;
 }
 
-var serverCode = getServerCode();
+const serverCode = getServerCode();
 
 function validatePassword(password) {
   if(password === serverCode)
     return 1;
   else
+  {
+    console.log('Password is wrong');
     return 0;
+  }
 }
 
 function sleep(milliseconds) {
@@ -79,6 +82,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
   var relay1Value = 0; //static variable for current status
   
   socket.on('takePicture', function(pwassWord, quality, width, height) {
+    console.log('Taking Picture');
     if(!validatePassword(pwassWord))
       return;
     var opts = { width: width, height: height, quality: quality, delay: 0,
@@ -90,13 +94,16 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
     var fileName  = 'images/image-' + Date.now();
     
     NodeWebcam.capture( fileName, opts, function( err, data ) {
-        socket.emit('pictureTaken', data);
+      if(err)
+        console.log(err);
+      socket.emit('pictureTaken', data);
     });
     Webcam.clear();
     //fs.unlinkSync(fileName);
   });
   
   socket.on('chargerDoor', function(data) {
+    console.log('Toggling Charger Door');
     if(!validatePassword(data))
       return;
     relay1.writeSync(1);
@@ -105,6 +112,7 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
   });
   
   socket.on('vanDoor', function(data) {
+    console.log('Toggling Van Door');
     if(!validatePassword(data))
       return;
     relay2.writeSync(1);
@@ -114,14 +122,32 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
 });
 
 process.on('SIGINT', cleanUp); //on ctrl+c
+process.on('SIGINT', function (){  //on ctrl+c
+  console.log('In SIGINT');
+  relay1.writeSync(0); // Turn relay1 off
+  relay2.writeSync(0); // Turn relay2 off
+  relay1.unexport(); // Unexport relay1 GPIO to free resources
+  relay2.unexport(); // Unexport relay2 GPIO to free resources
+  //unwatchAll();
+  process.exit(); //exit completely
+});
 
-process.on('uncaughtException', cleanUp); //on exception
-
+//process.on('uncaughtException', cleanUp); //on exception
+process.on('uncaughtException', function (err){ //on exception
+  console.log('In Uncuaght Exception');
+  console.log(err);
+  relay1.writeSync(0); // Turn relay1 off
+  relay2.writeSync(0); // Turn relay2 off
+  relay1.unexport(); // Unexport relay1 GPIO to free resources
+  relay2.unexport(); // Unexport relay2 GPIO to free resources
+  //unwatchAll();
+  process.exit(); //exit completely
+});
 function cleanUp(){
 	relay1.writeSync(0); // Turn relay1 off
 	relay2.writeSync(0); // Turn relay2 off
 	relay1.unexport(); // Unexport relay1 GPIO to free resources
 	relay2.unexport(); // Unexport relay2 GPIO to free resources
-	unwatchAll();
+	//unwatchAll();
 	process.exit(); //exit completely
 }
